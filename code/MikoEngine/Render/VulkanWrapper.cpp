6ab36c71e6d08b6fb4539a17b04b5988991e7a94
warzes <warzes@mail.ru>
2020-08-03 17:12:51 +1000

    -

---
+++ code/MikoEngine/Render/VulkanWrapper.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#if SE_VULKAN
+#include "VulkanWrapper.h"
+
+namespace wrapper
+{
+
+} // namespace wrapper
+#endif // SE_VULKAN
---
+++ code/MikoEngine/Render/VulkanWrapper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#if SE_VULKAN
+
+namespace wrapper
+{
+	class Instance
+	{
+	public:
+
+	};
+
+} // namespace wrapper
+#endif // SE_VULKAN
